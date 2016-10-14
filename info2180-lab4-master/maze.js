fe6594c410db22b8208ccde715dfe4feef1243b1
disilver9 <disilver9@gmail.com>
2016-10-14 12:06:48 -0500

    part2

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -3,14 +3,28 @@ window.onload = function main ()
 	
 	
 	
-	var edge = document.getElementById('boundary1');
+/**	var edge = document.getElementById('boundary1');
 	function mouseo(){
 		edge.setAttribute("class","youlose boundary");
 						};
 	
 	edge.onmouseover=function(e) {
 	        mouseo();
-	};
+	};**/
+	
+	var edges = document.querySelectorAll(".boundary");
+ 	var count=0;
+  
+ 	for(var i=0;i< edges.length;i++){
+ 		edges[i].onmouseover=function(){
+ 			count ++;
+ 			if(count>0){
+ 				for (var h=0;h< edges.length;h++){
+ 				edges[h].setAttribute("class","youlose boundary");
+ 				}
+ 			}
+ 		}
+ 	}
 	
 
 };
