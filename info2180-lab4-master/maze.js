dc944c4d415d664a1ab64cb6073a6df32083673c
disilver9 <disilver9@gmail.com>
2016-10-14 17:22:24 -0500

    Part 6

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -17,6 +17,8 @@ window.onload = function main ()
 	var end = document.getElementById('end');
  	var start=document.getElementById('start');
 	var msg=document.getElementById("status").textContent;
+	var checking=0;
+	var checkend=0;
   
  	for(var i=0;i< edges.length;i++)
 	{
@@ -37,9 +39,10 @@ window.onload = function main ()
 	
 end.onmouseover=function()
 {
+	checkend++;
 	if (count > 0){document.getElementById("status").innerHTML = "Try again ";}
 		else
-			{document.getElementById("status").innerHTML = "Congratulations ";}
+			{document.getElementById("status").innerHTML = "Congratulations, click S to start over ";}
 		
 	
 };
@@ -49,9 +52,42 @@ start.onclick=function()
 	{
 		edges[h].setAttribute("class","boundary")
 		count=0;
+		checking=2;
+		checkend=1;
 	}
 	document.getElementById("status").innerHTML = msg;
 };
+
+start.onmouseover=function()
+{
+	checking ++;
+	
+};
+
+
+
+document.body.onmouseover=function(num)
+{
+	var MainDiv=document.getElementById("maze");
+	var left=MainDiv.offsetLeft;
+	var right=MainDiv.offsetRight;
+	var moY=num.clientY;
+	var moX=num.clientX;
+	
+	if (moX < left && checking>1 )
+	{
+		for (var h=0;h< edges.length;h++)
+				{
+ 				edges[h].setAttribute("class","youlose boundary");
+ 				}
+		document.getElementById("status").innerHTML = "Loser, please click S to continue";
+		}
+	
+
+				
+}
+
+
 	
 
 };
