d93b7eedc16ad3cd872545810c082a8e34ff9b6d
disilver9 <disilver9@gmail.com>
2016-10-14 12:20:57 -0500

    part3

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -14,17 +14,32 @@ window.onload = function main ()
 	
 	var edges = document.querySelectorAll(".boundary");
  	var count=0;
+	var 
   
- 	for(var i=0;i< edges.length;i++){
- 		edges[i].onmouseover=function(){
+ 	for(var i=0;i< edges.length;i++)
+	{
+ 		edges[i].onmouseover=function()
+		{
  			count ++;
- 			if(count>0){
- 				for (var h=0;h< edges.length;h++){
+ 			if(count>0)
+			{
+ 				for (var h=0;h< edges.length;h++)
+				{
  				edges[h].setAttribute("class","youlose boundary");
  				}
  			}
  		}
  	}
+	
+	
+end.onmouseover=function()
+{
+	if (count > 0){alert ("Come on Bruh!!!");}
+		else
+			{alert ("Congrats!!!");}
+		
+	
+}
 	
 
 };
